4ce7bbce2a5fa36f06c2d6414820d351b8c01220
Draxprod <draxprod@gmail.com>
2016-11-05 16:49:59 +0100

    Source

---
+++ cfgClutter.h
@@ -0,0 +1,12 @@
+class DefaultClutter;
+class clutter
+{
+	class StrBigFallenBranches_pine: DefaultClutter
+	{
+		model = "A3\Plants_F\Clutter\c_bigFallenBranches_pine.p3d";
+		affectedByWind = 0.0;
+		swLighting = 0;
+		scaleMin = 0.3;
+		scaleMax = 0.7;
+	};
+};
